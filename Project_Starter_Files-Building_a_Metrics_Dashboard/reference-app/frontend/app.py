83e1b32cf8043efc90de22f470e90eca4b8cb727
binayokbhowmikxp <40661295+binayokbhowmikxp@users.noreply.github.com>
2022-04-11 18:56:50 +0530

    Update app.py

--- Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/frontend/app.py
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/frontend/app.py
@@ -1,10 +1,74 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
+import logging
+from jaeger_client import Config
+from jaeger_client.metrics.prometheus import PrometheusMetricsFactory
+from opentelemetry import trace
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.exporter import jaeger
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter
+
+
+from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
+from prometheus_flask_exporter import PrometheusMetrics
+
+def init_tracer(service):
+    logging.getLogger('').handlers = []
+    logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+
+    config = Config(
+        config={
+            'sampler': {
+                'type': 'const',
+                'param': 1,
+            },
+            'logging': True,
+        },
+        service_name=service,
+        validate=True
+    )
+
+    # this call also sets opentracing.tracer
+    return config.initialize_tracer()
+
+tracer = init_tracer('frontend')
 
 app = Flask(__name__)
 
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
+CORS(app)
+
+# add this missing from starter code
+metrics = GunicornInternalPrometheusMetrics(app, group_by='endpoint')
+
+# static information as metric
+metrics.info('frontend', 'Frontend App Metrics', version='1.0.3')
+
+# register extra metrics
+metrics.register_default(
+    metrics.counter(
+        'by_path_counter', 'Request count by request paths', labels={'path': lambda: request.path}
+    )
+)
+
+# custom metric to be applied to multiple endpoints
+endpoint_counter = metrics.counter(
+    'by_endpoint_counter', 'Request count by endpoints',
+    labels={'endpoint': lambda: request.endpoint}
+)
+
 
 @app.route("/")
+@endpoint_counter
 def homepage():
+    with tracer.start_span('front-end-homepage') as span:
+        span.set_tag('message', 'Welcome to the front end')
     return render_template("main.html")
 
 
